aa1e002e465b90d5b16069552a38ffa3908d6190
nitesh-official <niteshndj10@gmail.com>
2025-03-02 21:45:49 +0530

    Initilized Second Commit -> C Sharp Basics OOPS Concept Code Base

---
+++ Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Player
+    {
+        public string name = "Nitesh";
+        private int health = 60;
+
+        public int getHealth()
+        {
+            return health;
+        }
+
+        public void setHealth(int h)
+        {
+            health = h;
+        }
+
+        // Oops Concept :- Classes and Object
+
+        static void Main(String[] args)
+        {
+
+            Player tommy = new Player();
+
+            Console.WriteLine(tommy.getHealth());
+
+            tommy.setHealth(80);
+
+            Console.WriteLine(tommy.getHealth());
+
+        }
+
+
+
+    }
+}
