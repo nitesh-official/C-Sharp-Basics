6107457dceb67962303992bf79491f668ee1d02f
nitesh-official <niteshndj10@gmail.com>
2025-03-03 21:15:22 +0530

    CETPA Day-1 C#(Basics) Class Code

---
+++ Application/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Today First Program
+
+            // int x = 10;
+            // x += 5;
+
+            // Today Second Program
+
+            //int x = 5;
+            //x %= 3;
+            //Console.WriteLine(x);
+
+            // Today Third Program
+
+            //int x = 5;
+            //x >>= 3;
+            //Console.WriteLine(x);
+
+            // Today Fourth Program
+
+            //int x = 5;
+            //x <<= 3;
+            //Console.WriteLine(x);
+
+            // Today Fifth Program
+
+            //int x = 5;
+            //int y = 3;
+            //Console.WriteLine(x==y);
+
+            // Today Sixth Program
+
+            //int x = 5;
+            //int y = 3;
+            //Console.WriteLine(x > 3 &&  y < 10);
+
+            // Today Seventh Program
+
+            //int x = 5;
+            //Console.WriteLine(x > 3 || x < 4);
+
+            // Today Eight Program
+
+            //int x = 5;
+            //Console.WriteLine(!(x > 3 || x < 4));
+
+            // Today Nine Program
+
+            //string txt = "Nitesh";
+            //Console.WriteLine("Length of string " +txt.Length);
+
+            // Today Ten Program
+
+            //string txt = "Hello World";
+            //Console.WriteLine(txt.ToUpper());
+            //Console.WriteLine(txt.ToLower());
+
+            // Today Eleventh Program
+
+            //if(20 > 18)
+            //{
+            //    Console.WriteLine("20 is Greater than 18");
+            //}
+
+            // Today Twelve Program
+
+            int time = 11;
+
+            if (time < 12)
+            {
+                Console.WriteLine("Good Morning have a nice day...!!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Good Evening");
+                Console.ReadLine();
+            }
+
+
+            /* Todays All program Completed */
+
+
+        }
+    }
+}
