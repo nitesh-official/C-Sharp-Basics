33aace24b8615a279c8eed7fb705d59e25446a6a
nitesh-official <niteshndj10@gmail.com>
2025-03-07 19:37:53 +0530

    CETPA Day-5 C#(Basics) Class Code

---
+++ MyApplicationD5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApplicationD5
+{
+    //internal class Program
+    //{
+
+    //    static int PlusMethodint(int x,int y)
+    //    {
+    //        return x + y;
+    //    }
+    //    static double PlusMethoddouble(double x, double y)
+    //    {
+    //        return x + y;
+    //    }
+    //    static void Main(string[] args)
+    //    {
+
+    //        int myNum1 = PlusMethodint(8, 5);
+    //        double myNum2 = PlusMethoddouble(4.3, 6.26);
+
+    //        Console.WriteLine("Int:" + myNum1);
+    //        Console.WriteLine("Double:" + myNum2);
+
+    //    }
+    //}
+
+    //class car
+    //{
+    //    string color = "RED";
+
+    //    static void Main(string[] args)
+    //    {
+    //        car myobj1 = new car();
+
+
+    //        Console.WriteLine(myobj1.color);
+
+    //    }
+    //}
+
+    class car
+    {
+        string color = "RED";
+
+        static void Main(string[] args)
+        {
+            car myobj1 = new car();
+            car myobj2 = new car();
+
+            Console.WriteLine(myobj1.color);
+            Console.WriteLine(myobj2.color);
+
+
+        }
+    }
+}
